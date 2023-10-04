7ee631bac5b271f0be92b74bd83db51ff00048e5
Famouus Bernard <famousbernard@gmail.com>
2023-10-04 14:19:12 -0700

    successful

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('INPUT#btn_translate').click(function() {
+    const languageCode = $('INPUT#language_code').val();
+    const url = `https://www.fourtonfish.com/hellosalut/?lang=${languageCode}`;
+    
+    $.get(url, function(data) {
+      $('DIV#hello').text(data.hello);
+    });
+  });
+});
